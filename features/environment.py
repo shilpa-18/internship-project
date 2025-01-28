48551796d583b6878f0792878049ae49426eb862
Shilpa A <Shilpagt1808@gmail.com>
2025-01-28 12:38:22 -0500

    task 6

--- features/environment.py
+++ features/environment.py
@@ -7,6 +7,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from app.application import Application
 from support.logger import logger
 
+
 def browser_init(context, scenario_name):
     """
     :param context: Behave context
@@ -27,26 +28,37 @@ def browser_init(context, scenario_name):
     #     service=service
     # )
 
-    bs_user = 'shilpaa_mORtIz'
-    bs_key = 'EQYt38F8EvLGzh4Grz7W'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-
-    options = Options()
-    bstack_options = {
-        "os" : "Windows",
-        "osVersion" : "11",
-        'browserName': 'edge',
-        'sessionName': scenario_name,
+    mobile_emulation = {
+        "deviceName": "iPhone SE"  # You can use other device names as well
     }
 
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # bs_user = 'shilpaa_mORtIz'
+    # bs_key = 'EQYt38F8EvLGzh4Grz7W'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os" : "Windows",
+    #     "osVersion" : "11",
+    #     'browserName': 'edge',
+    #     'sessionName': scenario_name,
+    # }
+    #
+    #
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 15)
     context.app = Application(context.driver)
+
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
